95a3497e62d3e47bad3ee3b6d2830796c2b55e77
divyanshu-dj <jhadivyanshu042@gmail.com>
2025-03-30 00:30:25 +0530

    Dockerized the app and make it ready for deployment

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -3,7 +3,7 @@ import { Contest } from './types';
 import axios from 'axios';
 import { toast } from '@/components/ui/use-toast';
 
-const client =  'http://localhost:5000';
+const client = import.meta.env.VITE_API_URL_LOCAL || "http://localhost:5000";
 
 export const fetchContests = async ({ pageParam = 0 }): Promise<{ contests: Contest[]; nextOffset: number; hasMore: boolean }> => {
   try {
