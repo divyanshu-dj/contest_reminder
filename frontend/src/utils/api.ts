7192b1abd8ee1fff54a3f78858b8e67d135a9cac
divyanshu-dj <jhadivyanshu042@gmail.com>
2025-03-30 20:34:59 +0530

    fix related to api again :<

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -3,7 +3,7 @@ import { Contest } from './types';
 import axios from 'axios';
 import { toast } from '@/components/ui/use-toast';
 
-const client = "http://ec2-65-0-20-56.ap-south-1.compute.amazonaws.com:5000";
+const client = "http://dsabunnies.me:5000/:5000";
 
 export const fetchContests = async ({ pageParam = 0 }): Promise<{ contests: Contest[]; nextOffset: number; hasMore: boolean }> => {
   try {
